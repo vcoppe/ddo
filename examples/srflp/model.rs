5ab39d739bc5f787b852fc3a9be2776e57d66813
vcoppe <>
2020-12-14 11:27:01 +0100

    Back to SRFLP v1
    This reverts commit 280bacf7

--- examples/srflp/model.rs
+++ examples/srflp/model.rs
@@ -3,7 +3,7 @@ use std::ops::Not;
 
 use bitset_fixed::BitSet;
 
-use ddo::common::{Decision, Domain, Variable, VarSet, BitSetIter};
+use ddo::common::{Decision, Domain, Variable, VarSet};
 use ddo::abstraction::dp::Problem;
 use std::cmp::Reverse;
 
@@ -28,11 +28,29 @@ impl Hash for State {
 #[derive(Debug, Clone)]
 pub struct Srflp {
     pub g : Vec<Vec<isize>>,
-    pub l : Vec<isize>
+    pub l : Vec<isize>,
+    pub edges : Vec<(isize,usize,usize)>,
+    pub lengths : Vec<(isize,usize)>,
 }
 impl Srflp {
     pub fn new(g : Vec<Vec<isize>>, l : Vec<isize>) -> Srflp {
-        Srflp { g, l }
+        let mut edges = vec![];
+        let mut lengths = vec![];
+        for (i, is) in g.iter().enumerate() {
+            for (j, js) in is.iter().enumerate() {
+                if i < j {
+                    edges.push((*js, i, j));
+                }
+            }
+            lengths.push((l[i], i));
+        }
+        edges.sort_unstable_by_key(|&x| Reverse(x.0));
+        lengths.sort_unstable();
+        Srflp { g, l, edges, lengths }
+    }
+
+    fn no_vertex(&self) -> usize {
+        self.nb_vars()
     }
 }
 impl Problem<State> for Srflp {
@@ -53,34 +71,30 @@ impl Problem<State> for Srflp {
     }
 
     fn domain_of<'a>(&self, state: &'a State, _var: Variable) -> Domain<'a> {
-        Domain::from(&state.free)
+        let n = state.free.count_ones() as usize;
+        if n == 0 { // relaxed node with empty free vertices intersection
+            Domain::from(vec![self.no_vertex() as isize])
+        } else {
+            Domain::from(&state.free)
+        }
     }
 
-    fn transition(&self, state: &State, vars: &VarSet, d: Decision) -> State {
+    fn transition(&self, state: &State, _vars: &VarSet, d: Decision) -> State {
         let i = d.value as usize;
 
         let mut result = state.clone();
 
-        result.free.set(i, false);
-        result.cuts[i] = 0;
+        if i != self.no_vertex() {
+            result.free.set(i as usize, false);
+            result.cuts[i] = 0;
 
-        if state.free.count_ones() == 1 + vars.len() as u32 {
-            result.cut -= state.cuts[i];
-            for j in BitSetIter::new(&state.free) {
-                result.cut += self.g[i][j];
-                result.cuts[j] += self.g[i][j];
-            }
-        } else { // relaxed state
-            let mut cuts = vec![];
-            for j in BitSetIter::new(&state.free) {
-                result.cuts[j] += self.g[i][j];
-                cuts.push(result.cuts[j]);
-            }
-            // compute cut as the sum of the smallest cut values for remaining spots
-            cuts.sort_unstable();
-            result.cut = 0;
-            for j in 0..vars.len() {
-                result.cut += cuts[j];
+            for j in 0..self.nb_vars() {
+                if state.free[j] {
+                    result.cut += self.g[i][j];
+                    result.cuts[j] += self.g[i][j];
+                } else {
+                    result.cut -= self.g[i][j];
+                }
             }
         }
 
@@ -90,6 +104,10 @@ impl Problem<State> for Srflp {
     fn transition_cost(&self, state: &State, _vars: &VarSet, d: Decision) -> isize {
         let i = d.value as usize;
 
-        - self.l[i] * (state.cut - state.cuts[i]).max(0)
+        if i == self.no_vertex() {
+            0
+        } else {
+            - self.l[i] * (state.cut - state.cuts[i])
+        }
     }
 }
